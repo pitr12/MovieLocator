c25a0fb8f205d51a0493f3b76583d27d040ae9ec
Peter Dubec <peter.dubec93@gmail.com>
2014-04-10 21:54:40 +0200

    Modify movies_helper

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,5 +3,6 @@ module MoviesHelper
   def image_of(movie, msize)
     image_name = "movie_#{movie.id}.jpg"
     image_tag(image_name, alt: movie.title, class: "movie_image", width: msize)
+    image_tag(image_name, alt: movie.title, class: "movie_image", width: msize)
   end
 end
