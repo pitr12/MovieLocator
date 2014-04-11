371ca76177b5eabdfe70757fe47ebe669d11d51f
Peter Dubec <peter.dubec93@gmail.com>
2014-04-11 11:21:05 +0200

    Fix bug with missing images

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,7 +2,10 @@ module MoviesHelper
   # Returns the image for the given movie.
   def image_of(movie, msize)
     image_name = "movie_#{movie.id}.jpg"
-    image_tag(image_name, alt: movie.title, class: "movie_image", width: msize)
-    image_tag(image_name, alt: movie.title, class: "movie_image", width: msize)
+    if FileTest.exist?("#{Rails.root}/public/images/#{image_name}")
+      image_check = image_tag("/images/#{image_name}", alt: movie.title, class: "movie_image", width: msize)
+    else
+      image_check = image_tag("/images/missing.jpg", alt: movie.title, class: "movie_image", width: msize)
+    end
   end
 end
