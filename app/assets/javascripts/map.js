f4d7b6f555006ca357c81adc1f63f585293df650
Peter Dubec <peter.dubec93@gmail.com>
2014-06-04 00:31:02 +0200

    Change basic move_show layout

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -16,3 +16,43 @@ function loadScript() {
         'callback=initialize';
     document.body.appendChild(script);
 }
+
+function showmap(hash) {
+    $(document).ready(function(){
+        var raw_markers   = hash;
+        var gmaps_markers;
+
+        function createSidebarLi(json) {
+            return ("<li><a>" + json.name + "<\/a></li>");
+            };
+
+        function bindLiToMarker($li, marker){
+            $li.click(function(){
+                handler.getMap().setZoom(14);
+                marker.setMap(handler.getMap()); //because clusterer removes map property from marker
+                marker.panTo();
+                google.maps.event.trigger(marker.getServiceObject(), 'click');
+                $('html, body').animate({
+                    scrollTop: $("#map").offset().top
+                }, 500);
+        });
+        };
+
+        function createSidebar(){
+            for (var i=0;i<raw_markers.length;i++){
+            var $li = $( createSidebarLi(raw_markers[i]) );
+            $li.appendTo($('#markers_list'));
+            bindLiToMarker($li, gmaps_markers[i]);
+            }
+        };
+
+        handler = Gmaps.build('Google');
+              handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+                  gmaps_markers = handler.addMarkers(raw_markers);
+                  handler.bounds.extendWith(gmaps_markers);
+                  handler.fitMapToBounds();
+                  handler.getMap().setMapTypeId(google.maps.MapTypeId.HYBRID);
+                  createSidebar();
+              });
+          })
+}
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -15,11 +15,5 @@ module MoviesHelper
       return false
     else return true
     end
-
-    #uri = URI(movie.img)
-    #request = Net::HTPP.new uri.host
-    #response = request.request_head uri.path
-    #return response.code.to_i == 200
-
   end
 end
